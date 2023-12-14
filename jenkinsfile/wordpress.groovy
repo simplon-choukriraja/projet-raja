3cfa68d56ca317b78110bfc8658aa8060115fd16
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-14 22:28:23 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -87,19 +87,24 @@ pipeline {
                     curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
                     chmod 700 get_helm.sh
                     ./get_helm.sh
-                    '''
-                    //Ajout du repository pour Prometheus et Grafana, et mise à jour
-                    sh ('''
-                    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-                    helm repo update
                     ''')
-                    //Installation d'un Helm Chart dans un namespace monitoring
-                    sh ('''
-     }              helm install prometheus \
-                    prometheus-community/kube-prometheus-stack \
-                    --namespace projet-monitoring \
-                    --create-namespace  
-                    ''')
+                        //Ajout du repository pour Prometheus et Grafana, et mise à jour
+                        sh ('''
+                        helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+                        helm repo update
+                        ''')
+                            //Installation d'un Helm Chart dans un namespace monitoring
+                            sh ('''
+                            helm install prometheus \
+                            prometheus-community/kube-prometheus-stack \
+                            --namespace projet-monitoring \
+                            --create-namespace  
+                            ''')
+                            }
+                        }
+                }
+     
+     }                    
  
      post {
         always {
