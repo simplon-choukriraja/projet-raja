8272b370eb5164661d99ddf5a9c9986be9144179
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-14 20:10:32 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -21,58 +21,58 @@ pipeline {
         }
 
         
-        stage('Run Terraform Commands') {
-            steps {
-                script {
-                    dir('terraform') {
-                        sh 'terraform init'
-                        sh 'terraform apply -auto-approve'
+        //stage('Run Terraform Commands') {
+            //steps {
+                //script {
+                    //dir('terraform') {
+                        //sh 'terraform init'
+                        //sh 'terraform apply -auto-approve'
                         
-                    }
-                 }
-             }
-        }
+                    //}
+                 //}
+             //}
+        //}
 
-        stage('Add az get-credentials Kubernetes') {
-            steps {
-                script {
-                    dir('kubernetes') {
-                      sh 'az aks get-credentials --name myakscluster --resource-group projet'
-                    }
-                 }
-             }
-        }
+        //stage('Add az get-credentials Kubernetes') {
+            //steps {
+                //script {
+                    //dir('kubernetes') {
+                      //sh 'az aks get-credentials --name myakscluster --resource-group projet'
+                    //}
+                 //}
+             //}
+        //}
         
-        stage('Traefik avec Helm') {
-            steps {
-                script {
-                    dir('kubernetes') {
+        //stage('Traefik avec Helm') {
+            //steps {
+                //script {
+                    //dir('kubernetes') {
                        //AJOUTER LE RÉFÉRENTIEL HELM DE TRAEFIK AUX REPOSITORIES  
-                      sh 'helm repo add traefik https://helm.traefik.io/traefik'
-                      sh 'helm repo update'
+                      //sh 'helm repo add traefik https://helm.traefik.io/traefik'
+                      //sh 'helm repo update'
                        //DÉPLOYER TRAEFIK AVEC HELM
-                      sh 'helm install traefik traefik/traefik'
-                    }
-                }
-            }
-        }
+                      //sh 'helm install traefik traefik/traefik'
+                    //}
+                //}
+            //}
+        //}
         
         stage('Deploy App Wordpress end MariaDB with k8s') {
             steps {
                 script {
                     dir('kubernetes') {
                       sh 'kubectl create -f namespace.yml'  
-                      ssh 'kubectl apply -f deployment.wp.yml' 
-                      ssh 'kubectl apply -f deployment.mysql.yml'
-                      ssh 'kubectl apply -f ingress.yml'
-                      ssh 'kubectl apply -f service-mysql.yml'
-                      ssh 'kubectl apply -f middleware.yml'
-                      ssh 'kubectl apply -f pv.yml'
-                      ssh 'kubectl apply -f secret-wp.yml'
-                      ssh 'kubectl apply -f secret-mysql.yml'
-                      ssh 'kubectl apply -f service-wp.yml'
-                      ssh 'kubectl apply -f storageclass.yml'  
-                      ssh 'kubectl apply -f basicauth.yml'
+                      sh 'kubectl apply -f deployment.wp.yml' 
+                      sh 'kubectl apply -f deployment.mysql.yml'
+                      sh 'kubectl apply -f ingress.yml'
+                      sh 'kubectl apply -f service-mysql.yml'
+                      sh 'kubectl apply -f middleware.yml'
+                      sh 'kubectl apply -f pv.yml'
+                      sh 'kubectl apply -f secret-wp.yml'
+                      sh 'kubectl apply -f secret-mysql.yml'
+                      sh 'kubectl apply -f service-wp.yml'
+                      sh 'kubectl apply -f storageclass.yml'  
+                      sh 'kubectl apply -f basicauth.yml'
                       //ssh 'kubectl apply -f cert-manager.yml'
                           
                     }
