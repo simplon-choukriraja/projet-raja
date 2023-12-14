eece8019d705ffdc43047f067b0a2a8648baadf6
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-14 13:36:02 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -14,7 +14,6 @@ pipeline {
                 script {
                 // Eseguire l'autenticazione ad Azure utilizzando le credenziali di servizio
                 sh 'az login --service-principal -u 7ac0b3c5-acf9-4398-afdf-8d77fe0aaaaa -p JID8Q~JaaVpUZ9fcvovJAny263zoFtccGeva0aTw --tenant a2e466aa-4f86-4545-b5b8-97da7c8febf3'
-                sh 'az aks get-credentials --name myakscluster --resource-group projet-rj'
                     
                 }
             }
