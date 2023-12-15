0c2461e9bcf09e2408ebb7247f8c2c09eaea102c
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-15 20:43:38 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -89,7 +89,7 @@ pipeline {
                 script {
                     // Accedi al cluster Kubernetes (assicurati che Jenkins abbia le credenziali appropriate)
                     // e utilizza kubectl per ottenere il valore del secret
-                    sh 'kubectl create secret generic authsecret --from-literal=users=dXNlcjokYXByMSQwdERsbjBKZyR4LnlyUk8ubVltdm1mNmxUNG9rNWExCgo -n wordpress'
+                    def decodedSecret = sh(script: "kubectl get secret authsecret -n wordpress -o=jsonpath='{.data.users}' | base64 --decode", returnStdout: true).trim()
                 }
             }
         }
