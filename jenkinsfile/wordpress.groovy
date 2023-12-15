736372bacbae699809461c91cc9fcb279e655145
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-15 22:29:30 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -94,7 +94,6 @@ pipeline {
                     def username = sh(script: "kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o=jsonpath='{.data.username}' | base64 --decode", returnStdout: true).trim()
                     def password = sh(script: "kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o=jsonpath='{.data.password}' | base64 --decode", returnStdout: true).trim()
                     sh "curl -u ${username}:${password} https://wordpress.raja-ch.me"
-                    
                 }
             }
         }
