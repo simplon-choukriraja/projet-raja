3a1101456532b95bbf5e7835f9e36991efa0639f
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-21 10:07:58 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -112,11 +112,11 @@ pipeline {
             steps {
                 script {
                     // Utilizza l'API di Gandi per aggiornare il record DNS
-                    sh """
+                    sh '''
                     curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Apikey ${GANDI_API_KEY}' \\
                     -d '{\"rrset_ttl\": 10800, \"rrset_values\": [\"${env.TRAFFIK_IP}\"]}' \\
                     https://api.gandi.net/v5/livedns/domains/${DNS_ZONE}/records/${DNS_RECORD}/A
-                    """
+                    '''
                 }
             }
         }
