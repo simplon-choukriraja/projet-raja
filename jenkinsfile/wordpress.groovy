056fb5eddc70c4374733e9e38c4c319f8fdacfdc
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-22 21:16:46 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -74,6 +74,7 @@ pipeline {
             steps {
                 script {
                     dir('projet-raja/kubernetes') {
+                      sh 'ls -a'  
                       sh 'kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml'
                       sh 'kubectl create namespace wordpress'  
                       sh 'kubectl apply -f deployment-wp.yml' 
