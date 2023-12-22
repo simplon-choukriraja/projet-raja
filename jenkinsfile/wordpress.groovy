d08e998fb3c798c8436a5320600f479ab0c8010d
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-22 16:41:32 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -29,7 +29,6 @@ pipeline {
         stage('Clone Repository') {
             steps {
                 script {
-                    sh 'cd /tmp'
                     sh 'git clone https://github.com/simplon-choukriraja/projet-raja.git'
                 }
             }
@@ -39,7 +38,6 @@ pipeline {
             steps {
                 script {
                     dir('terraform') {
-                        sh 'cd /tmp/projet-raja/terraform'
                         sh 'terraform init'
                         sh 'terraform apply -auto-approve'
                         
