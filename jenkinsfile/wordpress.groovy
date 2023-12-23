98e6bf0dba39a23f4194a7c250f27f284c46c7ec
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-23 19:56:26 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -70,72 +70,72 @@ pipeline {
             }
         }
         
-        stage('Deploy App Wordpress end MariaDB with k8s') {
-            steps {
-                script {
-                    dir('kubernetes') {
-                      sh 'kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml'
-                      sh 'kubectl create namespace wordpress'  
-                      sh 'ls'
-                      sh 'kubectl apply -f deployment-mysql.yml'
-                      sh 'kubectl apply -f ingress.yml'
-                      sh 'kubectl apply -f service-mysql.yml'
-                      sh 'kubectl apply -f middleware.yml'
-                      sh 'kubectl apply -f pvc.yml'
-                      sh 'kubectl apply -f basicauth.yml'
-                      sh 'kubectl apply -f secret-mysql.yml'
-                      sh 'kubectl apply -f service-wp.yml'
-                      sh 'kubectl apply -f storageclass.yml'  
-                      sh 'kubectl apply -f cert-manager.yml'
+        //stage('Deploy App Wordpress end MariaDB with k8s') {
+            //steps {
+                //script {
+                    //dir('kubernetes') {
+                      //sh 'kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml'
+                      //sh 'kubectl create namespace wordpress'  
+                      //sh 'ls'
+                      //sh 'kubectl apply -f deployment-mysql.yml'
+                      //sh 'kubectl apply -f ingress.yml'
+                      //sh 'kubectl apply -f service-mysql.yml'
+                      //sh 'kubectl apply -f middleware.yml'
+                      //sh 'kubectl apply -f pvc.yml'
+                      //sh 'kubectl apply -f basicauth.yml'
+                      //sh 'kubectl apply -f secret-mysql.yml'
+                      //sh 'kubectl apply -f service-wp.yml'
+                      //sh 'kubectl apply -f storageclass.yml'  
+                      //sh 'kubectl apply -f cert-manager.yml'
                           
-                    }
-                 }
-            }
-        }
+                    //}
+                 //}
+            //}
+        //}
 
     
-        stage('Recover IP Traefik') {
-            steps {
-                script {
+        //stage('Recover IP Traefik') {
+            //steps {
+                //script {
                     // Esegue il comando kubectl per ottenere l'indirizzo IP del LoadBalancer
-                    def traffikIP = sh(script: "kubectl get svc ${SERVICE_NAME} -n ${NAMESPACE} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'", returnStdout: true).trim()
-                    echo "L'indirizzo IP di Traefik è: ${traffikIP}"
-                }
-            }
-        } 
+                    //def traffikIP = sh(script: "kubectl get svc ${SERVICE_NAME} -n ${NAMESPACE} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'", returnStdout: true).trim()
+                    //echo "L'indirizzo IP di Traefik è: ${traffikIP}"
+                //}
+            //}
+        //} 
 
-        stage('Mettre à jour l enregistrement DNS sur Gandi') {
-            steps {
-                script {
+        //stage('Mettre à jour l enregistrement DNS sur Gandi') {
+            //steps {
+                //script {
                     // Utilizza l'API di Gandi per aggiornare il record DNS
-                    sh ('''
-                    curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Apikey ${GANDI_API_KEY}' \\
-                    -d '{\"rrset_ttl\": 10800, \"rrset_values\": [\"${env.TRAFFIK_IP}\"]}' \\
-                    https://api.gandi.net/v5/livedns/domains/${DNS_ZONE}/records/${DNS_RECORD}/A
-                    ''')
-                }
-            }
-        }
+                    //sh ('''
+                    //curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Apikey ${GANDI_API_KEY}' \\
+                    //-d '{\"rrset_ttl\": 10800, \"rrset_values\": [\"${env.TRAFFIK_IP}\"]}' \\
+                    //https://api.gandi.net/v5/livedns/domains/${DNS_ZONE}/records/${DNS_RECORD}/A
+                    //''')
+                //}
+            //}
+        //}
 
-        stage('Auth-Secret') {
-            steps {
-                script {
+        //stage('Auth-Secret') {
+            //steps {
+                //script {
                     // Accedi al cluster Kubernetes (assicurati che Jenkins abbia le credenziali appropriate)
                     // e utilizza kubectl per ottenere il valore del secret
-                    def username = sh(script: "kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o=jsonpath='{.data.username}' | base64 --decode", returnStdout: true).trim()
-                    def password = sh(script: "kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o=jsonpath='{.data.password}' | base64 --decode", returnStdout: true).trim()
-                    sh 'curl -k ${username}:${password} https://wordpress.raja-ch.me'
-                }
-            }
-        }
-        stage ('Installation de Prometheus et Grafana via Helm') {
-            steps {
-                script {
+                    //def username = sh(script: "kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o=jsonpath='{.data.username}' | base64 --decode", returnStdout: true).trim()
+                    //def password = sh(script: "kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o=jsonpath='{.data.password}' | base64 --decode", returnStdout: true).trim()
+                    //sh 'curl -k ${username}:${password} https://wordpress.raja-ch.me'
+                //}
+            //}
+        //}
+        //stage ('Installation de Prometheus et Grafana via Helm') {
+            //steps {
+                //script {
                     // Installation de Prometheus et Grafana via Helm
-                    sh ('''
-                    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-                    chmod 700 get_helm.sh
-                    ./get_helm.sh
+                    //sh ('''
+                    //curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+                    //chmod 700 get_helm.sh
+                    //./get_helm.sh
                     ''')
                         // Ajout du repository pour Prometheus et Grafana, et mise à jour
                         sh ('''
@@ -152,20 +152,6 @@ pipeline {
                             }
                         }
                 }
-        
-        stage ('Loki') {
-            steps {
-                script {
-                    // Installation Loki
-                    sh ('''
-                    helm repo add grafana https://grafana.github.io/helm-charts
-                    helm repo update
-                    helm upgrade --install promtail --namespace projet-monitoring grafana/promtail
-                    helm upgrade --install loki --namespace projet-monitoring grafana/loki-stack
-                    ''')
-                }
-            }
-        }
      
      }                    
  
