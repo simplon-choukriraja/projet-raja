79c69c97c95fdef514354c640e885b0fa3befe49
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-23 20:49:59 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -20,7 +20,7 @@ pipeline {
             steps {
                 script {
                 // Eseguire l'autenticazione ad Azure utilizzando le credenziali di servizio
-                    sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID' 
+                    sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID' 
                     
                 }
             }
