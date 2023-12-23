55d41a41cf9a59bb15fac21ad3523c669c8921c4
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-23 22:48:59 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -71,53 +71,53 @@ pipeline {
             }
         }
         
-        stage('Deploy App Wordpress end MariaDB with k8s') {
-            steps {
-                script {
-                    dir('projet-raja/kubernetes') {
-                      sh 'kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml'
-                      sh 'kubectl create namespace wordpress'  
-                      sh 'kubectl apply -f deployment-mysql.yml'
-                      sh 'kubectl apply -f ingress.yml'
-                      sh 'kubectl apply -f service-mysql.yml'
-                      sh 'kubectl apply -f middleware.yml'
-                      sh 'kubectl apply -f pvc.yml'
-                      sh 'kubectl apply -f basicauth.yml'
-                      sh 'kubectl apply -f secret-mysql.yml'
-                      sh 'kubectl apply -f service-wp.yml'
-                      sh 'kubectl apply -f storageclass.yml'  
-                      sh 'kubectl apply -f cert-manager.yml'
+        //stage('Deploy App Wordpress end MariaDB with k8s') {
+            //steps {
+                //script {
+                    //dir('projet-raja/kubernetes') {
+                      //sh 'kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml'
+                      //sh 'kubectl create namespace wordpress'  
+                      //sh 'kubectl apply -f deployment-mysql.yml'
+                      //sh 'kubectl apply -f ingress.yml'
+                      //sh 'kubectl apply -f service-mysql.yml'
+                      //sh 'kubectl apply -f middleware.yml'
+                      //sh 'kubectl apply -f pvc.yml'
+                      //sh 'kubectl apply -f basicauth.yml'
+                      //sh 'kubectl apply -f secret-mysql.yml'
+                      //sh 'kubectl apply -f service-wp.yml'
+                      //sh 'kubectl apply -f storageclass.yml'  
+                      //sh 'kubectl apply -f cert-manager.yml'
                           
-                    }
-                 }
-            }
-        }
+                    //}
+                 //}
+            //}
+        //}
 
     
-        stage('Recover IP Traefik') {
-            steps {
-                script {
+        //stage('Recover IP Traefik') {
+            //steps {
+                //script {
                     //Esegue il comando kubectl per ottenere l'indirizzo IP del LoadBalancer
-                    def traffikIP = sh(script: "kubectl get svc ${SERVICE_NAME} -n ${NAMESPACE} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'", returnStdout: true).trim()
-                    echo "L'indirizzo IP di Traefik è: ${traffikIP}"
-                }
-            }
-        } 
+                    //def traffikIP = sh(script: "kubectl get svc ${SERVICE_NAME} -n ${NAMESPACE} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'", returnStdout: true).trim()
+                    //echo "L'indirizzo IP di Traefik è: ${traffikIP}"
+                //}
+            //}
+        //} 
 
-        stage('Mettre à jour l enregistrement DNS sur Gandi') {
-            steps {
-                script {
+        //stage('Mettre à jour l enregistrement DNS sur Gandi') {
+            //steps {
+                //script {
                     // Utilizza l'API di Gandi per aggiornare il record DNS
-                        withCredentials([azureServicePrincipal(credentialsId: 'GANDI_API_KEY', variable: 'API_KEY')]) {
-                        sh ('''
-                        curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Apikey ${GANDI_API_KEY}' \\
-                        -d '{\"rrset_ttl\": 10800, \"rrset_values\": [\"${env.TRAFFIK_IP}\"]}' \\
-                        https://api.gandi.net/v5/livedns/domains/${DNS_ZONE}/records/${DNS_RECORD}/A
-                        ''')
-                    }
-                }
-            }
-        }
+                        //withCredentials([azureServicePrincipal(credentialsId: 'GANDI_API_KEY', variable: 'API_KEY')]) {
+                        //sh ('''
+                        //curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Apikey ${GANDI_API_KEY}' \\
+                        //-d '{\"rrset_ttl\": 10800, \"rrset_values\": [\"${env.TRAFFIK_IP}\"]}' \\
+                        //https://api.gandi.net/v5/livedns/domains/${DNS_ZONE}/records/${DNS_RECORD}/A
+                        //''')
+                    //}
+                //}
+            //}
+        //}
 
         //stage('Auth-Secret') {
             //steps {
