3d26deac752489690c4bed104e063323be789cad
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-25 10:29:47 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -124,9 +124,11 @@ pipeline {
                      //Utilizza l'API di Gandi per aggiornare il record DNS
                         withCredentials([string(credentialsId: 'API_KEY', variable: 'GANDI_API_KEY')]) {
                         sh ('''
-                        curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Apikey ${GANDI_API_KEY}' \\
-                        -d '{\"rrset_ttl\": 10800, \"rrset_values\": [\"${env.TRAFFIK_IP}\"]}' \\
-                        https://api.gandi.net/v5/livedns/domains/${DNS_ZONE}/records/${DNS_RECORD}/A
+                        curl -X PUT \
+                             -H "Content-Type: application/json" \
+                             -H "Authorization: Apikey ${GANDI_API_KEY}" \
+                             -d '{"rrset_ttl": 10800, "rrset_values": ["'${env.TRAFFIK_IP}'"]}' \
+                             "https://api.gandi.net/v5/livedns/domains/raja-ch.me/records/www/A"
                         ''')
                     }
                 }
