4091f3db4c02f5857026222ae1bcbf00cf011ae0
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-27 16:17:09 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -169,27 +169,16 @@ pipeline {
             steps {
                 script {
                     dir('projet-raja/monitoring') { 
-                        withCredentials([usernamePassword(credentialsId: 'grafanapwd', usernameVariable: 'USERNAME', passwordVariable: 'GRAFANAPWD')]) {
-                        //sh 'kubectl delete -f grafana.yml'
-                        //sh 'kubectl delete -f prometheus.yml'
-                        //sh 'kubectl delete -f service-grafana.yml'
-                        //sh 'kubectl delete -f service-prometheus.yml'
-                        sh 'kubectl apply -f grafana.yml'
-                        sh 'kubectl apply -f prometheus.yml'
-                        sh 'kubectl apply -f service-grafana.yml'
-                        sh 'kubectl apply -f service-prometheus.yml'
-                        sh 'kubectl get pods -n wordpress'
-                        // Scrivi il segreto in un file in modo sicuro
-                        writeFile file: 'temp_pwd.txt', text: "${GRAFANAPWD}"
-
-                        // Leggi il contenuto del file temporaneo in una variabile e usa 'sed' per la sostituzione
-                        sh '''
-                        GRAFANAPWD=$(cat temp_pwd.txt)
-                        sed -i "s/GRAFANAPWD: grafanapwd/GRAFANAPWD: ${GRAFANAPWD}/" secret-grafana.yml
-                        rm temp_pwd.txt
-                        '''
-
-                        echo "The user is: ${USERNAME}"
+                        sh 'kubectl delete -f grafana.yml'
+                        sh 'kubectl delete -f prometheus.yml'
+                        sh 'kubectl delete -f service-grafana.yml'
+                        sh 'kubectl delete -f service-prometheus.yml'
+                        //sh 'kubectl apply -f grafana.yml'
+                        //sh 'kubectl apply -f prometheus.yml'
+                        //sh 'kubectl apply -f service-grafana.yml'
+                        //sh 'kubectl apply -f service-prometheus.yml'
+                        //sh 'kubectl get pods -n wordpress'
+                        
 
                         }
                     }
