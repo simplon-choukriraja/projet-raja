2446c00452173790eb54ffea1f16544ade833f2a
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-27 21:29:47 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -169,11 +169,11 @@ pipeline {
             steps {
                 script {
                     dir('projet-raja/monitoring') { 
-                        //sh 'kubectl delete -f grafana.yml'
-                        sh 'kubectl delete -f prometheus.yml'
-                        sh 'kubectl delete -f service-grafana.yml'
-                        sh 'kubectl delete -f service-prometheus.yml'
-                        //sh 'kubectl apply -f deployment-box.yml'
+                        sh 'kubectl apply -f grafana.yml'
+                        sh 'kubectl apply -f prometheus.yml'
+                        sh 'kubectl apply -f service-grafana.yml'
+                        sh 'kubectl apply -f service-prometheus.yml'
+                        sh 'kubectl apply -f deployment-box.yml'
                                              
                     }
                 }
