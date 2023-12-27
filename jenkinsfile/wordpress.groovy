d6b4e468437d8c21d520ba6f828684a85488c036
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2023-12-27 11:08:21 +0100

    Update wordpress.groovy

--- jenkinsfile/wordpress.groovy
+++ jenkinsfile/wordpress.groovy
@@ -165,7 +165,7 @@ pipeline {
         }
 
         
-        stage ('Installation of Prometheus and Grafana via Helm') {
+        stage('Installation of Prometheus and Grafana via Helm') {
             steps {
                 script {
                   dir('raja-projet/monitoring') { 
@@ -195,7 +195,7 @@ pipeline {
     }
      
 
-         stage ('Installation of Prometheus and Grafana with kubernetes') {
+         stage('Installation of Prometheus and Grafana with kubernetes') {
             steps {
                 script {
                   dir('projet-raja/monitoring') {
